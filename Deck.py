1510a6d3db8eac6d2b15f83abc690ec3dd2cfd01
derekcswong <derek4b4@gmail.com>
2020-09-23 13:46:23 -0700

    project struct

---
+++ Deck.py
@@ -0,0 +1,15 @@
+import Card
+
+class Deck:
+    """
+    Represents an instance of 52-card unique cards
+    """
+
+    def __init__(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def deal(self):
+        pass
